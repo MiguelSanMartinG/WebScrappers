ada5f5fbff413dc1598e5a41e12a7d7595ddf8ef
Miguel San Martin <miguelsan65@gmail.com>
2025-01-12 20:39:08 -0600

    ✨ REFACTOR (main.py, UnderArmourScanner.py): Simplifica la lógica de `guardar_producto` para insertar directamente productos, eliminando la verificación previa de existencia. Se añaden funciones para verificar si un producto ya existe (`check_producto_is_saved`) y si ya se escaneó hoy (`check_if_is_scannedToday`). Se refactoriza `consultar_producto_pornombre` a `get_idConsulta_with_Sku` y `generar_consulta_numeroResultados` a `createConsulta`, mejorando la claridad del código. Se elimina la impresión del sku del producto en `UnderArmourScanner`. ✨ FEAT (.idea/git_toolbox_prj.xml): Añade configuración para Git Toolbox.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from datetime import date
+from operator import truediv
 
 from entities import Producto
 from origins import UnderArmourScanner
@@ -14,36 +15,21 @@ conexion = psycopg2.connect(
 
 
 # Checa si un producto existe en la base de datos y devuelve su id si es asi si no devuelve None
-def consultar_producto_pornombre(producto):
+def get_idConsulta_with_Sku(producto):
     cursor = conexion.cursor()
-    today = date.today()
     try:
-            # Revisar si es un nuevo objeto
-            consulta = "select sku from productos where sku = %s;"
-            cursor.execute(consulta, [producto.sku])
-            sku = cursor.fetchone()
-            # Si sku contiene un registro
-            if sku is not None:
-                # Revisar si hoy ya se registro
-                consultar_ultima_fecha = """
-                select fecha_consulta from historicos_productos h
-                join public.consultas c on h.id_consulta = c.id_consulta
-                join public.productos p on p.id_producto = h.id_producto
-                where fecha_consulta=%s and p.sku=%s"""
-                cursor.execute(consultar_ultima_fecha, (today, producto.sku))
-                ultima_fecha = cursor.fetchone()
-                # si hay algo registrado
-                if ultima_fecha:
-                    return None
-                else:
-                    #si no devuelve vacio
-                    return sku
-
-            return sku
+        # Revisar si hoy ya se registro
+        id_producto = """
+        select id_producto from productos 
+        where sku=%s
+        """
+        cursor.execute(id_producto, (producto.sku,))
+        id = cursor.fetchone()
+        return id[0]
 
 
     except Exception as e:
-        print("Error en la consulta: ", e)
+        print("Error en la consulta: 32 ", e)
         conexion.rollback()
     finally:
         cursor.close()
@@ -52,32 +38,23 @@ def consultar_producto_pornombre(producto):
 def guardar_producto(producto, categoria):
     cursor = conexion.cursor()
     try:
-        id = consultar_producto_pornombre(producto)
-        # print(resultado)
-        if id:
-            print("Producto no guardado porque ya existente." + producto.get_titulo())
-            return id[0]
-        else:
-            # Insertar el producto si no existe
             insertar_producto = """
              INSERT INTO productos (nombre, tipo, enlace, imagen, categoria, sku)
-             VALUES (%s, %s, %s, %s, %s, %s) RETURNING id_producto;
+             VALUES (%s, %s, %s, %s, %s, %s)
              """
+            print(producto.get_titulo(), producto.get_tipo(), producto.get_enlace(), producto.imagen, categoria, str(producto.sku))
+            cursor.execute(insertar_producto, (producto.get_titulo(), producto.get_tipo(), producto.get_enlace(), producto.imagen, categoria, str(producto.sku)))
 
-            cursor.execute(insertar_producto, (producto.get_titulo(), producto.get_tipo(), producto.get_enlace(), producto.imagen, categoria, producto.sku))
-            id = cursor.fetchone()
+            # id = cursor.fetchone()
             conexion.commit()
-            print("Producto guardado correctamente.")
-            # id = consultar_producto_pornombre(producto)
-            return id[0]
-
     except Exception as e:
-        print("Error en la consulta: linea 50 main", e)
+        print("Error en la consulta: 54" , e)
         conexion.rollback()
     finally:
+        print("Producto guardado correctamente.")
         cursor.close()
 
-def generar_consulta_numeroResultados(lista):
+def createConsulta(lista):
     cursor = conexion.cursor()
     try:
         fecha = lista[0].date()
@@ -92,7 +69,7 @@ def generar_consulta_numeroResultados(lista):
         conexion.commit()
         return id[0]
     except Exception as e:
-        print("Error en la consulta: ", e)
+        print("Error en la consulta: 72 ", e)
         conexion.rollback()
     finally:
         cursor.close()
@@ -113,34 +90,76 @@ def hacer_historico_producto(id_consulta,id_producto, item:Producto):
         print("Historico guardado correctamente. " +str(id_historico[0]))
         return id_historico[0]
     except Exception as e:
-        print("Error en la consulta: ", e)
+        print("Error en la consulta: 93", e)
+        conexion.rollback()
+    finally:
+        cursor.close()
+def check_producto_is_saved(producto):
+    cursor = conexion.cursor()
+    try:
+            consulta = "select count(*) from productos where sku = %s"
+            cursor.execute(consulta, (str(producto.sku),))
+            count = cursor.fetchone()[0]
+            if count > 0:
+                print("Producto existente.")
+                return True
+            else:
+                # print("Producto no existente")
+                return False
+
+    except Exception as e:
+        print("Error en la consulta: linea 111 main", e)
+        conexion.rollback()
+    finally:
+        cursor.close()
+def check_if_is_scannedToday(item):
+    cursor = conexion.cursor()
+    today = date.today()
+    try:
+        consultar_ultima_fecha = """
+                select fecha_consulta from historicos_productos h
+                join public.consultas c on h.id_consulta = c.id_consulta
+                join public.productos p on p.id_producto = h.id_producto
+                where fecha_consulta=%s and p.sku=%s"""
+        cursor.execute(consultar_ultima_fecha, (today, item.sku))
+        ultima_fecha = cursor.fetchone()
+        if ultima_fecha:
+            print("El producto necesita esperar 24 antes de volver a checarse.")
+            return False
+        else:
+            return True
+
+    except Exception as e:
+        print("Error en la consulta: 133 ", e)
         conexion.rollback()
     finally:
         cursor.close()
 
+
+
 def comenzar_consulta(sexo, categoria):
     lista = UnderArmourScanner.getProducts(sexo, categoria)
-    id_consulta = generar_consulta_numeroResultados(lista)
-    print(id_consulta)
-    lista = lista[2:]
+    id_consulta = createConsulta(lista)
+    lista = lista[2:] # Se borra la fecha y el número de resultados.
+
     for item in lista:
-        print(item)
-        id_producto = guardar_producto(item,categoria)
-        id_historico = hacer_historico_producto(id_consulta, id_producto, item)
-        print(id_historico)
+        id_producto = None
+        valid = False
+        existente = check_producto_is_saved(item)
+        if existente:
+            valid = check_if_is_scannedToday(item)
+        else:
+            guardar_producto(item, categoria)
+            valid=True
+
+        if valid:
+            id_producto = get_idConsulta_with_Sku(item)
+            id_historico = hacer_historico_producto(id_consulta, id_producto, item)
+            print(id_historico)
+        else:
+            print("Registro no realizado")
 
 
-# comenzar_consulta('hombre', 'ropa/ropa-de-abrigo')
-# comenzar_consulta('hombre', 'pants-y-leggings')
-# comenzar_consulta('hombre', 'ropa/sudaderas-con-y-sin-capucha')
-# comenzar_consulta('hombre', 'ropa/sin-mangas')
-# comenzar_consulta('hombre', 'tenis/sandalias-chanclas')
-# comenzar_consulta('hombre', 'ropa/manga-larga')
-# comenzar_consulta('hombre', 'ropa/tops')
 comenzar_consulta('hombres', 'baselayer')
-# comenzar_consulta('outlet','/hombre/accesorios/')
-# comenzar_consulta('outlet','/hombre/pants/')
-# comenzar_consulta('mens','/clothing/performance-shirts/')
-
 
 conexion.close()
